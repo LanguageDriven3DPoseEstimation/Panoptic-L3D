7a4ef3453553a288a76c3dd03824a5b5bf7c3d8a
LanguageDriven3DPoseEstimation <languagedriven3dpe@163.com>
2024-08-14 20:37:25 +0800

    Add files via upload

---
+++ configs/init_config.py
@@ -0,0 +1,19 @@
+from yacs.config import CfgNode as CN
+
+###########################
+# Config definition
+###########################
+
+_C = CN()
+
+_C.INPUT = CN()
+
+_C.MODEL = CN()
+
+_C.TRAIN = CN()
+
+_C.TEST = CN()
+
+_C.VAL = CN()
+
+_C.DATA = CN()
